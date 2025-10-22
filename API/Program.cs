af8c632a52cb9d01b61213961dec820fe47400ac
Marquis B-Cole <Browncom@gmatc.edu>
2025-10-21 21:29:25 -0500

    Complete Activity 7: React Frontend Setup & Product Display

--- API/Program.cs
+++ API/Program.cs
@@ -8,7 +8,19 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddDbContext<DataContext>();
 
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowReactApp");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
